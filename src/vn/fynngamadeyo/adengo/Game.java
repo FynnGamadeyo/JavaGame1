ccdcac86da5143a3a1a05f56ff72efaafe371e59
Nam <huynh@tzi.de>
2014-06-27 21:48:35 +0200

    --

--- src/vn/fynngamadeyo/adengo/Game.java
+++ src/vn/fynngamadeyo/adengo/Game.java
@@ -58,18 +58,34 @@ public class Game extends Canvas implements Runnable{
 		Manager manager = new Manager();
 		
 		long lastTime = System.nanoTime();
+		long timer =  System.currentTimeMillis();
+		
 		final double ns = 1000000000.0 / 60.0;
 		double delta=0;
+		int frames=0;
+		int updates=0;
+		
 		while(running){
 			long now = System.nanoTime();
 			delta += (now-lastTime) / ns;
 			lastTime=now;
 			while(delta >=1){
 				manager.update();
+				updates++;
 				delta--;
 			}
 			render();
+			frames++;
+			
+			if((System.currentTimeMillis() - timer) > 1000 ){
+				timer +=1000;
+				System.out.println(updates + " ups, "+ frames + " fps");
+				frame.setTitle(updates + " ups, "+ frames + " fps");
+				frames=0;
+				updates=0;
+			}
 		}
+		stop();
 	}
 	
 	public void render(){
